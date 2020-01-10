2f6ccadb4149e2b446424be475bd04613eb6f548
Pranjal Sarkar <Pranjal.Sarkar@neudesic.com>
2020-01-10 13:30:20 +0530

    Updates

---
+++ precache-manifest.3589de8f60b9cc0930acb677fb00a502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aea5a0433ee52ff3cfd336ff586dce4",
+    "url": "/first-github/index.html"
+  },
+  {
+    "revision": "e5318d80843f320b0803",
+    "url": "/first-github/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "561065f8aff51ad6e392",
+    "url": "/first-github/static/js/2.b4d14c4a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/first-github/static/js/2.b4d14c4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5318d80843f320b0803",
+    "url": "/first-github/static/js/main.7722963d.chunk.js"
+  },
+  {
+    "revision": "e9194045bd75d47be5e5",
+    "url": "/first-github/static/js/runtime-main.f6748803.js"
+  }
+]);
